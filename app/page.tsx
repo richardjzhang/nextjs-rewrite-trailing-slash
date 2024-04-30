1324dc7762d4add38ba86395f42ad5cd4487a78c
richardjzhang <hello@richardjzhang.com>
2024-04-30 19:06:30 +1000

    Add links comparing with slash and without slash

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,22 @@ export default function Home() {
   return (
     <main className="flex flex-col gap-2 p-8">
       <Link href="/docs" className="mt-2 underline text-sky-400">
-        Go to docs
+        Go to Docs without slash
       </Link>
       <Link href="/blog" className="mt-2 underline text-sky-400">
-        Go to blog
+        Go to Blog without slash
       </Link>
       <Link href="/posts" className="mt-2 underline text-sky-400">
-        Go to posts
+        Go to Posts without slash
+      </Link>
+      <Link href="/docs/" className="mt-2 underline text-sky-400">
+        Go to Docs with slash
+      </Link>
+      <Link href="/blog/" className="mt-2 underline text-sky-400">
+        Go to Blog with slash
+      </Link>
+      <Link href="/posts/" className="mt-2 underline text-sky-400">
+        Go to Posts with slash
       </Link>
     </main>
   );
