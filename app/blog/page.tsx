5c18a95a301870515db896afc32bf2b1e7a8f8ac
richardjzhang <hello@richardjzhang.com>
2024-04-30 18:12:07 +1000

    Add pages

---
+++ app/blog/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Blog() {
+  return (
+    <div>
+      <h1>Blog</h1>
+      <p>Welcome to my blog!</p>
+      <Link href="/">Back home</Link>
+    </div>
+  );
+}
---
+++ app/docs/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Docs() {
+  return (
+    <div>
+      <h1>Docs</h1>
+      <p>Welcome to docs!</p>
+      <Link href="/">Back home</Link>
+    </div>
+  );
+}
