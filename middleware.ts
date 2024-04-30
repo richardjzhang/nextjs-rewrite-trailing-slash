d6cf69234d54b568722bd6af8e6f205995eb0f3e
richardjzhang <hello@richardjzhang.com>
2024-05-01 08:28:26 +1000

    Fix example

--- middleware.ts
+++ middleware.ts
@@ -5,16 +5,16 @@ const legacyPrefixes = ["/docs", "/blog"];
 export default async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
-  if (legacyPrefixes.some((prefix) => pathname.startsWith(prefix))) {
+  if (
+    legacyPrefixes.some((prefix) => pathname.startsWith(prefix)) ||
+    pathname === "/"
+  ) {
     return NextResponse.next();
   }
 
-  // apply trailing slash handling
-  if (
-    !pathname.endsWith("/") &&
-    !pathname.match(/((?!\.well-known(?:\/.*)?)(?:[^/]+\/)*[^/]+\.\w+)/)
-  ) {
-    req.nextUrl.pathname += "/";
-    return NextResponse.redirect(req.nextUrl);
+  // remove trailing slash
+  if (pathname.endsWith("/")) {
+    const newPathname = pathname.slice(0, -1);
+    return NextResponse.redirect(new URL(`${newPathname}`, req.nextUrl));
   }
 }
